2eb5ddd158304f4ad84750e66aa782a2d3186517
boguss1225 <44974259+boguss1225@users.noreply.github.com>
2019-01-14 23:39:49 +0900

    no message

--- GUI-Template/src/tpl/view/menubar/Dialogs.java
+++ GUI-Template/src/tpl/view/menubar/Dialogs.java
@@ -3,9 +3,7 @@ package tpl.view.menubar;
 import java.awt.BorderLayout;
 import java.awt.Dialog;
 import java.awt.FileDialog;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -18,6 +16,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
 
 import tpl.model.Model;
 import tpl.view.AdjustDialog;
@@ -125,7 +124,7 @@ class exitDialog{
 	 	
 	 	/*announcement */
 	 	JLabel l1 = new JLabel("Will you EXIT this program?");
-	 	l1.setHorizontalAlignment(l1.CENTER);
+	 	l1.setHorizontalAlignment(SwingConstants.CENTER);
 		/*set button1*/
 		JButton b1 = new JButton("OK");
 		class b1EventHandler implements ActionListener{
