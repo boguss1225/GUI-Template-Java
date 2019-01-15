858cb8ef547d2addf182ff84d0a53f571e702e97
daum oh <odu.wave@gmail.com>
2019-01-15 16:17:41 +0900

    참조문구 중복단어 수정, 다움채호 카피라이터에 작성

--- GUI-Template/src/tpl/view/middle/MiddlePanel.java
+++ GUI-Template/src/tpl/view/middle/MiddlePanel.java
@@ -4,17 +4,17 @@
  * Copyright (C) 2019  H.M. YOON, D.U. OH, S.H. CHAE.
  *
  * Java GUI Template is free software: you can redistribute it and/or modify
- * it under the terms of the GNU GPL General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 1 of the License, or
  * any later version.
  *
  * Java GUI Template is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, See the
- * GNU GPL General Public License for more details.
+ * GNU General Public License for more details.
  * it would be glad to be notified any further modification to copyrighters .
  *
- * You should have received a copy of the GNU GPL General Public License
+ * You should have received a copy of the GNU General Public License
  * along with Java GUI Template.  If not, see <http://www.gnu.org/licenses/>.
  */
 
